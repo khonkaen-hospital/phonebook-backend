c03664d2a646fd33fa007c330fe157e3bcfb827d
Premyos Deyadee <premyos.de@gmail.com>
2020-10-26 14:24:40 +0700

    first commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,30 @@
+import { PhonesModel } from '../models/phones';
+const phones = new PhonesModel();
+
+async function routes(app: any, opts: any, next: any) {
+
+    app.post('/', async (req: any, res: any) => {
+        const data = req.body.data;
+        let numberType = data.numberType;
+        let results = [];
+        if (numberType == '2') {
+            results = await phones.findTelemed(app.knex, data);
+        } else if (numberType == '1') {
+            results = await phones.find(app.knex, data);
+        }
+        res.status(200).send({ status: 'ok', result: results[0], page: results[1], totalpage: results[2] });
+    });
+
+    app.get('/department', async (req: any, res: any) => {
+        let results = await phones.getDepartment(app.knex);
+        res.status(200).send({ status: 'ok', result: results[0] });
+    })
+
+    app.post('/info', async (req: any, res: any) => {
+        let data = req.body.data;
+        let result = await phones.getInfo(app.knex, data);
+        res.status(200).send({ status: 'ok', result: result[0] });
+    })
+
+}
+module.exports = routes;
---
+++ src/models/phones.ts
@@ -0,0 +1,70 @@
+
+import Knex from "knex";
+import { compileFunction } from "vm";
+
+export class PhonesModel {
+
+    public perPage: number = 25;
+
+    async items(db: Knex, where: any, table: any) {
+        let _where = '';
+        if (where != '') {
+            _where = where;
+        }
+        let sql = `select count(*) as total from ${table} ${_where} `;
+        return await db.raw(sql);
+    }
+
+    async find(db: Knex, data: any) {
+        let table = 'phone_internal';
+        let _page = (data.page - 1);
+        let page = (data.page == 0) ? (_page * 1) : (_page * this.perPage);
+        let search = data.search;
+        let where = '';
+        let order = `order by no limit ${page}, ${this.perPage}`;
+        if (search != '') {
+            where = `where (no like '${search}%' or room like '%${search}%' or build like '%${search}%')`;
+            order = ``;
+        }
+        let sql = `select * from ${table} ${where} ${order};`;
+        let result = await db.raw(sql);
+        let allitems = await this.items(db, where, table);
+        let _total = Math.ceil(parseInt(allitems[0][0]['total']) / this.perPage)
+        return [result[0], data.page, _total]
+    }
+
+    async findTelemed(db: Knex, data: any) {
+        let table = 'telemedicine.telephone';
+        let _page = (data.page - 1);
+        let page = (data.page == 0) ? (_page * 1) : (_page * this.perPage);
+        let search = data.search;
+        let where = `where isactive = '1'`;
+        let order = `order by telephone limit ${page}, ${this.perPage}`;
+        if (search != '') {
+            let word = search.split(' ');
+            if (word.length > 1) {
+                where += `and (location like '%${word[0]}%' and hospname like '%${word[1]}%')`;
+            } else {
+                where += `and (telephone like '${search}%' or location like '%${search}%' or hospname like '%${search}%')`;
+            }
+            order = ``;
+        }
+        let sql = `select ref, hospcode, telephone as no, location as room, hospname as build, province from ${table} ${where} ${order};`;
+        let result = await db.raw(sql);
+        let allitems = await this.items(db, where, table);
+        let _total = Math.ceil(parseInt(allitems[0][0]['total']) / this.perPage)
+        return [result[0], data.page, _total];
+    }
+
+    async getDepartment(db: Knex) {
+        let sql = `select ref as id, name from hospdata.lib_office where isactive = '1';`;
+        return await db.raw(sql);
+    }
+
+    async getInfo(db: Knex, code: number) {
+        let sql = `select code, no, id, name, surname from hospdata.employee where code = '${code}';`;
+        return await db.raw(sql);
+
+    }
+
+}
