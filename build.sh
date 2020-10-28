0b26f9c4dc8a1d439b38243680dd588db76302da
Sathit Seethaphon <dixonsatit@gmail.com>
2020-10-28 10:22:33 +0700

    init deployment

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t docker.pkg.github.com/khonkaen-hospital/phonebook-backend/phonebook-api .
-docker push docker.pkg.github.com/khonkaen-hospital/phonebook-backend/phonebook-api
